84ee912a663dc2c940c70585c6bd1e22ddfcfb34
Fynn Petersen-Frey <fynn.petersen-frey@uni-hamburg.de>
2022-12-16 07:48:44 +0000

    fine-tune SBERT

--- main.py
+++ main.py
@@ -2,25 +2,40 @@ from pathlib import Path
 from typing import List
 import torch
 import pandas as pd
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, InputExample, losses
 from sentence_transformers.util import cos_sim
 from dos.dataset import SemEvalDataset, DIMENSIONS
-
+from torch.utils.data import DataLoader
 from torch.nn.functional import cosine_similarity
 
 pd.set_option('display.precision', 2)
 
+models = ["bert-base-multilingual-cased", "sentence-transformers/stsb-xlm-r-multilingual",
+          "sentence-transformers/LaBSE", "all-MiniLM-L6-v2", "all-mpnet-base-v2"]
+
+
 def main():
-    dataset = SemEvalDataset(Path("data/eval.csv"), Path("data/eval_data"))
-    for model_name in ["bert-base-multilingual-cased", "sentence-transformers/stsb-xlm-r-multilingual", "sentence-transformers/LaBSE", "all-MiniLM-L6-v2", "all-mpnet-base-v2"]:
+    dataset = SemEvalDataset(Path("data/train.csv"), Path("data/train_data"))
+    for model_name in models:
         try:
-            eval_model(model_name, dataset)
+            model = SentenceTransformer(model_name)
+            finetune_model(model, dataset)
+            eval_model(model, dataset, model_name)
         except Exception as e:
             print("Error evaluating", model_name)
             print(e)
 
-def eval_model(model_name, dataset):
-    model = SentenceTransformer(model_name)
+def normalize_score(one2four: float):
+    return 1 - 2 * (one2four - 1) / 3
+
+def finetune_model(model: SentenceTransformer, dataset: SemEvalDataset):
+    training_data: List[InputExample] = [InputExample(
+        texts=[pair.article_1.text, pair.article_2.text], label=normalize_score(pair.overall)) for pair in dataset]
+    train_dataloader = DataLoader(training_data, shuffle=True, batch_size=32)
+    train_loss = losses.CosineSimilarityLoss(model)
+    model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=1, warmup_steps=100)
+
+def eval_model(model: SentenceTransformer, dataset: SemEvalDataset, model_name: str):
     sims = []
     gold_geography: List[float] = []
     gold_entities: List[float] = []
@@ -31,7 +46,8 @@ def eval_model(model_name, dataset):
     gold_tone: List[float] = []
 
     for pair in dataset:
-        embeddings = model.encode([pair.article_1.text, pair.article_2.text], convert_to_tensor=True)
+        embeddings = model.encode(
+            [pair.article_1.text, pair.article_2.text], convert_to_tensor=True)
         sim = cosine_similarity(embeddings[0], embeddings[1], dim=0)
         sims.append(1 - sim.item())
         gold_geography.append(pair.geography)
@@ -52,12 +68,12 @@ def eval_model(model_name, dataset):
         torch.tensor(gold_tone),
     ))
     corrs = torch.corrcoef(similiarities)
-    df = pd.DataFrame(corrs, columns=["predict"] + DIMENSIONS, index=["predict"] + DIMENSIONS)
-    df = df[["predict", "GEO", "ENT", "TIME", "NAR", "STYLE", "TONE", "Overall"]].reindex(["predict", "GEO", "ENT", "TIME", "NAR", "STYLE", "TONE", "Overall"])
+    df = pd.DataFrame(
+        corrs, columns=["predict"] + DIMENSIONS, index=["predict"] + DIMENSIONS)
+    df = df[["predict", "GEO", "ENT", "TIME", "NAR", "STYLE", "TONE", "Overall"]].reindex(
+        ["predict", "GEO", "ENT", "TIME", "NAR", "STYLE", "TONE", "Overall"])
     print(f"Stats for {model_name}")
     print(df)
-
-        
 
 
 if __name__ == "__main__":
