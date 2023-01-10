65e015b19f4ff642233d32b7d851c9bd4c050fa9
Hans Ole Hatzel <hhatzel@gmail.com>
2023-01-10 18:14:53 +0100

    Extend fasttext based correlation

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from typing import List
 import pandas as pd
 from itertools import count
 import typer
+from enum import Enum
 from typing import Optional
+from collections import defaultdict
 import fasttext
 import torch
 import spacy
@@ -23,51 +25,83 @@ app = typer.Typer()
 nlp = spacy.load("en_core_web_lg")
 
 
-def extract_embeddings(text, embedder, only_verbs=True):
+def extract_embeddings(text, embedder, word_filter):
     doc = nlp(text)
     for token in doc:
-        if not only_verbs or token.pos_ == "VERB":
+        if word_filter(token):
             yield token.vector, embedder.get_word_vector(token.text)
 
+# KNOWN_NER_TAGS = 'ORDINAL', 'GPE', 'LOC', 'MONEY', 'CARDINAL', 'QUANTITY', 'LAW', 'DATE', 'NORP', 'EVENT', 'LANGUAGE', 'ORG', 'PERSON', 'TIME', 'PRODUCT', 'PERCENT', 'FAC', 'WORK_OF_ART'
 
-@app.command()
-def verbs(limit: Optional[int] = None, split: str = "test"):
+class WordKind(Enum):
+    ENTS = "entities"
+    ALL_NER = "ALL_NER"
+    VERB = "verb"
+    ALL = "all"
+    TIME = "time"
+    GEO = "geo"
+
+    def get_filter(self):
+        if self == WordKind.ALL:
+            return lambda token: True
+        elif self == WordKind.VERB:
+            return lambda token: token.pos_ == "VERB"
+        elif self == WordKind.ENTS:
+            return lambda token: token.ent_type_ in ["ORG", "PERSON", "GPE", "LOC", "LANGUAGE", "PRODUCT", "WORK_OF_ART", "FAC"]
+        elif self == WordKind.ALL_NER:
+            return lambda token: token.ent_type_ != ""
+        elif self == WordKind.TIME:
+            return lambda token: token.ent_type_ == "TIME"
+        elif self == WordKind.GEO:
+            return lambda token: token.ent_type_ in ["LOC", "GPE"]
+        else:
+            raise ValueError("Invalid variant", self)
+
+
+class DataSubset(Enum):
+    EN = "en"
+    ALL_TRANSLATED = "translated"
+
+    def get_data_path(self, split):
+        if self == DataSubset.EN:
+            return Path(f"data/{split}_data")
+        elif self == DataSubset.ALL_TRANSLATED:
+            return Path(f"data/{split}_data_translated")
+
+
+@app.command(name="fasttext")
+def fasttext_similarity(limit: Optional[int] = None, split: str = "test", embeddings: str = "cc.en.300.bin", kind: WordKind = WordKind.ALL, subset: DataSubset = "en"):
     current_dataset = None
     if split == "test":
-        test = SemEvalDataset(Path("data/eval.csv"), Path("data/eval_data"))
+        test = SemEvalDataset(Path("data/eval.csv"), subset.get_data_path("eval"), langs=["en"] if subset == subset.EN else "all")
         current_dataset = test
     elif split == "train":
-        train = SemEvalDataset(Path("data/train.csv"), Path("data/train_data"))
+        train = SemEvalDataset(Path("data/train.csv"), subset.get_data_path("train"), langs=["en"] if subset == subset.EN else "all")
         current_dataset = train
     else:
         raise ValueError("Invalid split")
-    embedder = fasttext.load_model("cc.en.300.bin")
+    embedder = fasttext.load_model(embeddings)
     predicted_sims = []
-    gold_sims_nar = []
-    gold_sims_ent = []
-    gold_sims = []
-    for only_verbs in [True, False]:
-        for pair, i in zip(current_dataset, range(limit) if limit is not None else count()):
-            verbs_a = [fasttext_emb for spacy_emb, fasttext_emb in extract_embeddings(pair.article_1.text, embedder, only_verbs=only_verbs)]
-            verbs_b = [fasttext_emb for spacy_emb, fasttext_emb in extract_embeddings(pair.article_2.text, embedder, only_verbs=only_verbs)]
-            if len(verbs_a) == 0 or len(verbs_b) == 0:
-                continue
-            verbs_a_stacked = np.stack(verbs_a)
-            verbs_b_stacked = np.stack(verbs_b)
-            sim = cos_sim(verbs_a_stacked, verbs_b_stacked)
-            sim.fill_diagonal_(0)
-            predicted_sims.append(sim.max(0).values.mean() + sim.max(1).values.mean())
-            # predicted_sims.append(sim.mean())
-            gold_sims_ent.append(pair.entities)
-            gold_sims_nar.append(pair.narrative)
-            gold_sims.append(pair.overall)
-        corrs = torch.corrcoef(torch.stack([torch.tensor(predicted_sims), torch.tensor(gold_sims)]))
-        corrs_narr = torch.corrcoef(torch.stack([torch.tensor(predicted_sims), torch.tensor(gold_sims_nar)]))
-        corrs_ent = torch.corrcoef(torch.stack([torch.tensor(predicted_sims), torch.tensor(gold_sims_ent)]))
-        print("#### ", "Only Verbs" if only_verbs else "All Words")
-        print("Overall correlation", corrs[0,1])
-        print("Narrative correlation", corrs_narr[0,1])
-        print("Entity correlation", corrs_ent[0,1])
+    gold_sims = defaultdict(list)
+    for pair, i in zip(current_dataset, range(limit) if limit is not None else count()):
+        verbs_a = [fasttext_emb for spacy_emb, fasttext_emb in extract_embeddings(pair.article_1.text, embedder, kind.get_filter())]
+        verbs_b = [fasttext_emb for spacy_emb, fasttext_emb in extract_embeddings(pair.article_2.text, embedder, kind.get_filter())]
+        if len(verbs_a) == 0 or len(verbs_b) == 0:
+            continue
+        verbs_a_stacked = np.stack(verbs_a)
+        verbs_b_stacked = np.stack(verbs_b)
+        sim = cos_sim(verbs_a_stacked, verbs_b_stacked)
+        sim.fill_diagonal_(0)
+        predicted_sims.append(sim.max(0).values.mean() + sim.max(1).values.mean())
+        for key, value in [("Geography", pair.geography), ("Entities", pair.entities), ("Time", pair.time), ("Narrative", pair.narrative), ("Overall", pair.overall), ("Style", pair.style), ("Tone", pair.tone)]:
+            gold_sims[key].append(value)
+    correlations = {}
+    for key, gold_values in gold_sims.items():
+        correlations[key] = torch.corrcoef(torch.stack([torch.tensor(predicted_sims), torch.tensor(gold_values)]))
+    print("#### Only considering", kind, "on", subset)
+    for key, corrs in correlations.items():
+        print(f"{key} correlation {-corrs[0,1].item():.2f}")
+
 
 @app.command()
 def main():
